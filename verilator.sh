cd8e0006718e9ad93dff6f27d9dce608956a993d
weisrc <weihengjiang64@hotmail.com>
2022-04-13 23:10:21 -0400

    fixed ci

--- verilator.sh
+++ verilator.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo docker pull verilator/verilator:latest >/dev/null
-sudo docker run -ti -v ${PWD}:/work --user $(id -u):$(id -g) verilator/verilator:latest "${@:1}"
+sudo docker run -ti -v $(pwd):/work --user $(id -u):$(id -g) verilator/verilator:latest "${@:1}"
