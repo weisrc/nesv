5b460d0e786357ba20115c7bd550835149c8f2b2
weisrc <weihengjiang64@hotmail.com>
2022-04-17 23:42:10 -0400

    better column 1

--- src/cpu/columns/1.sv
+++ src/cpu/columns/1.sv
@@ -8,22 +8,14 @@ function void column_1(inout state_t state);
     PTR_Y(state, 1);
     END(state, 6, 2);
   end
-  case (state.op[7:4])
-    'h0: ORA(state, 4);
-    'h1: ORA(state, 3);
-    'h2: AND(state, 4);
-    'h3: AND(state, 3);
-    'h4: EOR(state, 4);
-    'h5: EOR(state, 3);
-    'h6: ADC(state, 4);
-    'h7: ADC(state, 3);
-    'h8: STA(state, 4);
-    'h9: STA(state, 3);
-    'hA: LDA(state, 4);
-    'hB: LDA(state, 3);
-    'hC: CMP(state, 4);
-    'hD: CMP(state, 3);
-    'hE: SBC(state, 4);
-    'hF: SBC(state, 3);
+  case (state.op[7:5])
+    0: ORA(state, 4);
+    1: AND(state, 4);
+    2: EOR(state, 4);
+    3: ADC(state, 4);
+    4: STA(state, 4);
+    5: LDA(state, 4);
+    6: CMP(state, 4);
+    7: SBC(state, 4);
   endcase
 endfunction
